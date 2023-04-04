1f9257cfc2c7f839fb6b891ac4154b66407c66e5
Sonu Keshri <sonu.keshri@tredence.com>
2023-04-04 16:58:02 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    project="Sample_Project",
+    packages=find_packages(),
+    version='0.1.0',
+    description="A short description of your project",
+    author="Your Name",
+    license='MIT',
+)
